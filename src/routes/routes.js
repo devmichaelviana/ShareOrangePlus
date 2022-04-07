538eda6b6c7b60e258cd04549d09dcd2c40edef3
Michael Viana <vmichael116@gmail.com>
2022-04-07 00:56:25 -0300

    trabalhando o responsivo da tela de login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/home";
 import PageLogin from "../pages/login";
-import Page2 from "../pages/tela2";
 
 
 function routes() {
   return(
     <Routes>
       <Route  path="/" element={<Home />}/>
-      <Route  path="/login" element={<PageLogin />}/>
-      <Route  path="/tela" element={<Page2/>}/>        
+      <Route  path="/login" element={<PageLogin />}/>        
     </Routes>
   )
 }
