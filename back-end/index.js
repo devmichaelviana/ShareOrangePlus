65af2f8f5dafe8bd93cc0d82c533c77b7abacabd
fran <francielebernardo88@hotmail.com>
2022-04-08 09:58:23 -0300

    melhorias do back end

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const users = require('./routes/users')
+const Mentorias = require('./routes/mentorias')
 
 
 
@@ -17,12 +19,14 @@ app.use(
 app.use(express.json({limit: '50mb'}))
 app.use(cors())
 
-const users = require('./routes/users')
+
 app.use('/users', users)
-const Mentorias = require('./routes/Mentorias')
 app.use('/montoria', Mentorias)
 
-mongoose.connect('mongodb+srv://HackathonFCamara:v6oqXPB7xz8s3qgo@hackathon-fcamar.y71xw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+const login = process.env.LOGIN_DB;
+const senha = process.env.SENHA_DB
+
+mongoose.connect(`mongodb+srv://${login}:${senha}@hackathon-fcamar.y71xw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 .then(() => {
     console.log('connectamos ao mongoDB')
     app.listen(8080)
