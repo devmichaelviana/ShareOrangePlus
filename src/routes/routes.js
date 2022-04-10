2888a4c970f99e8977eace7dfc94449b1b88d7f7
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-10 14:06:56 -0300

    feat: adionando rota modal

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import PageLogin from "../pages/Logint";
-import PageCadastro from "../pages/Cadastro";
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import PageLogin from '../pages/Logint';
+import PageCadastro from '../pages/Cadastro';
+import ModalExemplos from '../pages/ModalExemplos';
 
 function routes() {
-  return(
+  return (
     <Routes>
-      <Route  path="/" element={<Home />}/>
-      <Route  path="/login" element={<PageLogin />}/>
-      <Route  path="/cadastro" element={<PageCadastro/>}/>        
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<PageLogin />} />
+      <Route path="/cadastro" element={<PageCadastro />} />
+      <Route path="/modal" element={<ModalExemplos />} />
     </Routes>
-  )
+  );
 }
 
 export default routes;
